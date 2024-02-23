ea190a78278e68531f7ff3ba56f0d5a935c9c097
Altti Äyräs <altti.ayras@gmail.com>
2024-02-23 17:42:52 +0200

    generate random tower

--- back/server.js
+++ back/server.js
@@ -3,12 +3,41 @@ const app = express()
 const cors = require('cors')
 app.use(cors())
 
-app.get("/api/hello", (req, res)=>{
+app.get("/api/randomtower", (req, res)=>{
+    const towers = [
+    "Dart Monkey",
+    "Boomerang Monkey",
+    "Bomb Shooter",
+    "Tack Shooter",
+    "Ice Monkey",
+    "Glue Gunner",
+    "Sniper Monkey",
+    "Monkey Sub",
+    "Monkey Buccaneer",
+    "Monkey Ace",
+    "Heli Pilot",
+    "Mortar Monkey",
+    "Dartling Gunner",
+    "Wizard Monkey",
+    "Super Monkey",
+    "Ninja Monkey",
+    "Alchemist",
+    "Druid",
+    "Banana Farm",
+    "Spike Factory",
+    "Monkey Village",
+    "Engineer Monkey",
+    "Beast Handler"
+    ]
+    let type = towers[Math.floor(Math.random() * towers.length)]
+    let tier = Math.floor(Math.random() * 6);
+    let path = Math.floor(Math.random() * 3)+1;
     res.json({
-        "testdata":"hello"
+        "type":type,
+        "tier":tier,
+        "path":path
     })
 })
-
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: 'unknown endpoint' })
