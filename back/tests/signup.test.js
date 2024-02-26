2b325ea6d4921ae0fd7980a7ffd26961566fdf45
Altti Äyräs <altti.ayras@gmail.com>
2024-02-26 19:25:21 +0200

    test for duplicate accounts

--- back/tests/signup.test.js
+++ back/tests/signup.test.js
@@ -2,6 +2,7 @@ const supertest = require('supertest')
 const app = require('../app')
 const User = require('../schemas/user')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 const api = supertest(app)
 
@@ -12,6 +13,9 @@ const usersInDb = async () => {
 
 beforeEach(async () => {
     await User.deleteMany({})
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'alreadyexists', passwordHash })
+    await user.save()
 })
 
 test('creating new account succeeds', async () => {
@@ -26,7 +30,6 @@ test('creating new account succeeds', async () => {
         .post('/api/signup')
         .send(newUser)
         .expect(201)
-        .expect('Content-Type', /application\/json/)
 
     const usersAtEnd = await usersInDb()
 
@@ -34,6 +37,24 @@ test('creating new account succeeds', async () => {
 
     const usernames = usersAtEnd.map(u => u.username)
     expect(usernames).toContain(newUser.username)
+})
+
+test('creating duplicate account fails', async () => {
+    const usersAtStart = await usersInDb()
+
+    const newUser = {
+        username: 'alreadyexists',
+        password: 'ValidPassword123!',
+    }
+
+    await api
+        .post('/api/signup')
+        .send(newUser)
+        .expect(400)
+
+    const usersAtEnd = await usersInDb()
+
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
 })
 
 test('creating new account with invalid username fails', async () => {
