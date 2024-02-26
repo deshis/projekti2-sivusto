6b4d6ca4898e36c22db8839d4279f97008927d35
Eversti <evert.makinen@gmail.com>
2024-02-26 19:29:54 +0200

    Basic connection to backend established in front

--- front/app/src/App.js
+++ front/app/src/App.js
@@ -1,7 +1,28 @@
-const App = () => (
-  <div>
-    <p>Definitely the app ever!</p>
-  </div>
-)
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const App = () => {
+  
+  const [monkey, setMonkey] = useState()
+  
+  useEffect(() => {
+    axios
+      .get('https://projekti2-sivusto.onrender.com/api/randomtower')
+      .then(response => {
+        console.log('promise fulfilled')
+        console.log(response.data.type)
+        setMonkey(response.data.type)      
+      })
+  }, [])
+
+  
+  
+  return(
+    <div>
+      <h1>Definitely the app ever!</h1>
+      <p>{monkey}</p>
+    </div>
+  )
+}
 
 export default App
