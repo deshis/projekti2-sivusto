37393d8c78d1aec18c63341c2c414640afc528f9
Altti Äyräs <altti.ayras@gmail.com>
2024-03-03 17:49:56 +0200

    add base tower description to towerdata

--- back/tests/towers.test.js
+++ back/tests/towers.test.js
@@ -34,6 +34,7 @@ test('random tower tier is between 0-5', async () => {
 
     expect(typeof response.body.type).toBe("string")
     expect(typeof response.body.category).toBe("string")
+    expect(typeof response.body.description).toBe("string")
     expect(Array.isArray(response.body.upgrades)).toBe(true)
   })
 
